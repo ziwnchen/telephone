ede3608e458f3a0bed93d2530ffa9789aaa96a84
ziwnchen <ziwn.chen@gmail.com>
2021-12-21 23:09:21 -0800

    Update change

--- client/intro/InstructionStepOne.jsx
+++ client/intro/InstructionStepOne.jsx
@@ -11,9 +11,9 @@ export default class InstructionStepOne extends React.Component {
         <div className="instructions">
           <h1> Task Instructions </h1>
           <p>
-            There are three different tasks you have to complete. Task 1 and
-            Task 2 will last for 9 rounds. Task 3 has 3 rounds.
-            There will be 21 rounds in total.
+            There are two different tasks you have to complete: Task 1 and
+            Task 2. Both tasks will last for 9 rounds. Along with instruction rounds,
+            there are 20 rounds in total.
           </p>
           <p>
             For task 1, you will have five seconds to read a sentence.
@@ -29,10 +29,8 @@ export default class InstructionStepOne extends React.Component {
             you only write one sentence.
           </p>
           <p>
-            For task 3, you will read the first few sentences of three stories.
-            Your task will be to write the next sentence in each of these stories.
-            You can write whatever you want, as long as it follows from
-            the previous sentences and as long as you only write one sentence.
+            You will be given the instructions seperately at the beginning of each
+            task.
           </p>
           <p>
             When you are ready to continue, press ‘Next’.
